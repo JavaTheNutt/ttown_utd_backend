050351a8b9cda289c733707d99798835ef6d94e5
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-08-25 16:28:40 +0100

    Created package-info.java for dto package

---
+++ src/main/java/ie/wit/model/dto/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains all of the DTO's (Data Transfer Objects) for both incoming and outgoing objects to be read from/written to the database 
+ *
+ * @author Joe Wemyss
+ */
+package ie.wit.model.dto;
