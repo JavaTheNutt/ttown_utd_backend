2eb4a16dafc4b94b89cffe0b105edfd7767a035c
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-08-25 16:15:30 +0100

    Updated LoginDto: added javadoc comments

--- src/main/java/ie/wit/model/dto/in/LoginDto.java
+++ src/main/java/ie/wit/model/dto/in/LoginDto.java
@@ -1,16 +1,75 @@
-
+package ie.wit.model.dto.in;
+/**
+ * This class represents the data that will be passed into the login controller when the user attempts to log in. Should match 
+ * to a UserEntity
+ * 
+ * @see ie.wit.model.entity.UserEntity UserEntity
+ * @author Joe Wemyss
+ */
 public class LoginDto
 {
+	/**
+	 * The email address that is passed in
+	 * Cannot be null or empty
+	 */
 	@NotNull
 	@NotEmpty
 	private String emailAddress;
 	
+	/**
+	 * The password that is passed in
+	 * Cannot be null or empty
+	 */
 	@NotNull
 	@NotEmpty
 	private String password;
 	
+	/**
+	 * The default constructor used by jackson to create the entity from JSON
+	 */
+	public LoginDto(){
+		
+	}
+	
+	/**
+	 * Constructor
+	 * @param String emailAddress the email address
+	 * @param String password the users password
+	 */
 	public LoginDto(String emailAddress, String password){
 		this.emailAddress = emailAddress;
+		this.password = password;
+	}
+	
+	/**
+	 * Accessor for emailAddress
+	 * @return String emailAddress
+	 */
+	public String getEmailAddress(){
+		return emailAddress;
+	}
+	
+	/**
+	 * Accessor for password
+	 * @return String password
+	 */
+	public String getPassword(){
+		return password;
+	}
+	
+	/**
+	 * Mutator for emailAddress
+	 * @param String emailAddress
+	 */
+	public void setEmailAddress(String emailAddress){
+		this.emailAddress = emailAddress;
+	}
+	
+	/**
+	 * Mutator for password
+	 * @param String password
+	 */
+	public void setPassword(String password){
 		this.password = password;
 	}
 }
