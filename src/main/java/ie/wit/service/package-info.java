84278211e039d267375b1ea90fbb8414b1106a6b
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-08-25 13:53:07 +0100

    Created package-info.java for service package

---
+++ src/main/java/ie/wit/service/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains all of the service classes that act as an itermediary between the repositories and controllers
+ *
+ * @author Joe Wemyss
+ */
+package ie.wit.model.service;
