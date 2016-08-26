789871a920e9dd26eda45e615107f6256d44751e
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-08-26 14:56:31 +0100

    Updated ExceptionHandler.java
    Made methods more generic; added support for PasswordMismatchException

--- src/main/java/ie/wit/exceptions/ExceptionHandler.java
+++ src/main/java/ie/wit/exceptions/ExceptionHandler.java
@@ -6,9 +6,50 @@ package ie.wit.exceptions
  */
 public class ExceptionHandler{
   
+  	/**
+  	 * Handle UserNotFoundException
+  	 * 
+  	 * @param req  the request that triggered the exception
+  	 * @param e  the exception that was thrown
+  	 * @return  a response entity containing the exception
+  	 */
 	@ExceptionHandler(UserNotFoundException.class)
 	public ResponseEntity<ClientErrorInformation> handleUserNotFound(HttpServletRequest req, Exception e){
-    	ClientErrorInformation error = new ClientErrorInformation(e.toString(), req.getRequestURI());
-    	return new ResponseEntity<ClientErrorInformation>(error, HttpStatus.UNAUTHORIZED);
+    	return handleUnauthorized(req, e);
+	}
+	
+	/**
+  	 * Handle PasswordMismatchException.
+  	 * 
+  	 * @param req  the request that triggered the exception
+  	 * @param e  the exception that was thrown
+  	 * @return  a response entity containing the exception
+  	 */
+	@ExceptionHandler(PasswordMismatchException.class)
+	public ResponseEntity<ClientErrorInformation> handlePasswordMismatch(HttpServletRequest req, Exception e){
+		return handleUnauthorized(req, e);
+	}
+	
+	/**
+	 * Handle any exception that needs to throw a Status code 401.
+	 * 
+	 * @param req  the request that triggered the exception
+  	 * @param e  the exception that was thrown
+  	 * @return  a response entity containing the exception
+	 */
+	private ResponseEntity<ClientErrorInformation> handleUnauthorized(HttpServletRequest req, Exception e){
+		return handleException(req, e, HttpStatus.UNAUTHORIZED);
+	}
+	/**
+	 * Handle any exception.
+	 * 
+	 * @param req  the request that triggered the exception
+  	 * @param e  the exception that was thrown
+  	 * @param status  the status to be returned
+  	 * @return  a response entity containing the exception
+	 */
+	private ResponseEntity<ClientErrorInformation> handleException(HttpServletRequest req, Exception e, HttpStatus status){
+		ClientErrorInformation error = new ClientErrorInformation(e.toString(), req.getRequestURI());
+    	return new ResponseEntity<ClientErrorInformation>(error, status);
 	}
 }
