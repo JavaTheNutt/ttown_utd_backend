dac6203f240fc8f271eafdbddbea18215a1c3295
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-08-26 08:58:32 +0100

    Updated UserNotFoundException: removed Status code

--- src/main/java/ie/wit/exceptions/UserNotFoundException.java
+++ src/main/java/ie/wit/exceptions/UserNotFoundException.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  *
  * @author Joe Wemyss
  */
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class UserNotFoundException extends RuntimeException
 {
 	public UserNotFoundException()
