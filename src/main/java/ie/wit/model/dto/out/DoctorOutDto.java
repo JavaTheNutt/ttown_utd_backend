cdd100de075d8b377ddcbe23ecdc58b1cc4b4993
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-08-26 11:27:23 +0100

    Built out DoctorOutDto.java

--- src/main/java/ie/wit/model/dto/out/DoctorOutDto.java
+++ src/main/java/ie/wit/model/dto/out/DoctorOutDto.java
@@ -6,5 +6,165 @@ package ie.wit.model.dto.out;
 * @author Joe Wemyss
 */
 public class DoctorOutDto{
+	/**
+	 * Representation of the primary key.
+	 * Cannot be null;
+	 */
+	@NotNull
+	private Long id;
+	
+	/**
+	 * The docotrs first name.
+	 */
+	private String firstName;
+	
+	/**
+	 * The docotrs surname.
+	 */
+	private String surname;
+	
+	/**
+	 * The docotrs street address.
+	 */
+	private String streetAddress;
+	
+	/**
+	 * The docotrs town address.
+	 * Cannot be null.
+	 * Defaults to "Thomastown" in the database
+	 */
+	 @NotNull
+	private String townAddress;
+	/**
+	 * The doctors contactNumber.
+	 */
+	private String contactNumber;
+	
+	/**
+	 * Default constructor
+	 */
+	public DoctorOutDto(){}
+	
+	/**
+	 * Full constructor
+	 */
+	public DoctorOutDto(Long id, String firstName, String surname, String streetAddress, String townAddress, String contactNumber){
+		this.id = id;
+		this.firstName = firstName;
+		this.surname = surname;
+		this.streetAddress = streetAddress;
+		this.townAddress = townAddress;
+		this.contactNumber = contactNumber;
+	}
+	
+	/**
+	 * Accessor for primary key.
+	 * 
+	 * @return  the primary key
+	 */
+	public Long getId(){
+		return id;
+	}
+	
+	/**
+	 * Accessor for firstName.
+	 * 
+	 * @return  doctor first name, wrapped in a java 8 Optional to prevent NPE
+	 */
+	public Optional<String> getFirstName(){
+		return Optional.ofNullable(firstName);
+	}
+	
+	/**
+	 * Accessor for surname.
+	 * 
+	 * @return  doctor surname, wrapped in a java 8 Optional to prevent NPE
+	 */
+	public Optional<String> getSurname(){
+		return Optional.ofNullable(surname);
+	}
 
+	/**
+	 * Accessor for streetAddress.
+	 * 
+	 * @return  doctor street address, wrapped in a java 8 Optional to prevent NPE
+	 */
+	public Optional<String> getStreetAddress(){
+		return Optional.ofNullable(streetAddress);
+	}
+	
+	/**
+	 * Accessor for townAddress.
+	 * 
+	 * @return  doctor town address
+	 */
+	public String getTownAddress(){
+		return townAddress;
+	}
+	
+	/**
+	 * Accessor for contactNumber
+	 * 
+	 * @return  doctor contact number, wrapped in a java 8 Optional to prevent NPE
+	 */
+	public Optional<String> getContactNumber(){
+		return Optional.ofNullable(contactNumber);
+	}
+	
+	/**
+	 * Mutator for primary key.
+	 * 
+	 * @param id  the primary key
+	 */
+	public void setId(Long id){
+		this.id = id;
+	}
+	
+	/**
+	 * Mutator for firstName.
+	 * 
+	 * @param firstName  the doctors first name
+	 */
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+	
+	/**
+	 * Mutator for surname.
+	 * 
+	 * @param surname  the doctors surname
+	 */
+	public void setSurname(String surname){
+		this.surname = surname;
+	}
+	
+	/**
+	 * Mutator for streetAddress.
+	 * 
+	 * @param streetAddress  the doctors street address
+	 */
+	public void setStreetAddress(String streetAddress){
+		this.streetAddress = streetAddress;
+	}
+	
+	/**
+	 * Mutator for townAddress.
+	 * Sets to defualt value if null.
+	 * 
+	 * @param townAddress  the doctors town address
+	 */
+	public void setTownAddress(String townAddress){
+		this.townAddress = townAddress == null ? "Thomastown" : townAddress;
+	}
+	
+	/**
+	 * Mutator for contactNumber.
+	 * 
+	 * @param contactNumber  the doctors contact number
+	 */
+	public void setContactNumber(String contactNumber){
+		this.contactNumber = contactNumber;
+	}
+	
+	// TODO: Generate toString()
 }
