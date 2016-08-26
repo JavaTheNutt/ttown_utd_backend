c9388e823367d59e6ed3ccc17dc2869ef06781a1
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-08-26 10:49:37 +0100

    Created DoctorOutDto.java

---
+++ src/main/java/ie/wit/model/dto/out/DoctorOutDto.java
@@ -0,0 +1,10 @@
+package ie.wit.model.dto.out;
+
+/**
+* This will be the representation of a doctor that will be sent out via the doctor endpoint
+*
+* @author Joe Wemyss
+*/
+public class DoctorOutDto{
+
+}
