12bb8ae9e5898acac2961be18b7ce439aff29170
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-08-26 09:07:45 +0100

    Updated ExceptionHandler.java:
    Added support for UserNotFoundException

--- src/main/java/ie/wit/exceptions/ExceptionHandler.java
+++ src/main/java/ie/wit/exceptions/ExceptionHandler.java
@@ -1,2 +1,14 @@
 package ie.wit.exceptions
-
+/**
+ * This class handles exceptions, wraps them in a ResponseEntity and returns them to the client
+ * 
+ * @author Joe Wemyss
+ */
+public class ExceptionHandler{
+  
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<ClientErrorInformation> handleUserNotFound(HttpServletRequest req, Exception e){
+    	ClientErrorInformation error = new ClientErrorInformation(e.toString(), req.getRequestURI());
+    	return new ResponseEntity<ClientErrorInformation>(error, HttpStatus.UNAUTHORIZED);
+	}
+}
