91cc6bbd98ced2150fca15746c55aa61712d2871
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-08-29 14:09:33 +0100

    Updated package-info.java for access package

--- src/main/java/ie/wit/service/access/package-info.java
+++ src/main/java/ie/wit/service/access/package-info.java
@@ -1,4 +1,6 @@
 /**
- * This package contains all of the services for
+ * This package contains all of the services for authentication. This most of the methods belonging to classes in this package will be private,
+ * with some being package-local to expose their fuctionality to other classes in the package. The LoginService.java is the only class to contain
+ * public methods and will act as the sole route in and out of this package.
  */
 package ie.wit.service.access;
