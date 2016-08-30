8bae5904433d096245866c130ae34b51825e8ef3
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-08-30 12:06:59 +0100

    Update UserOutDto.java: built class

--- src/main/java/ie/wit/model/dto/out/UserOutDto.java
+++ src/main/java/ie/wit/model/dto/out/UserOutDto.java
@@ -1,5 +1,61 @@
 package ie.wit.model.dto.out;
 
-public class UserOutDto {
-
+/**
+ * This class is the details of a user that will be sent back after a successful login.
+ * 
+ * @author Joe Wemyss
+ */
+public class UserOutDto 
+{
+  /**
+   * The users email address
+   */
+  private String emailAddress;
+  /**
+   * The users first name if present, otherwise unknown
+   */
+  private String firstName;
+  /**
+   * The users surname if present, otherwise unknown
+   */
+  private String surname;
+  
+  /**
+   * Constructor.
+   * This object will only be able to be created by passing user details from the database to the constructor.
+   * 
+   * @param user  a UserEntity retrieved from the database
+   */
+  public UserOutDto(UserEntity user){
+  	this.emailAddress = user.getEmailAddress();
+  	this.firstName = user.getFirstName().orElse("Unknown"); //if does not exist, set value to unknown
+  	this.surname = user.getSurname().orElse("Unknown");//if does not exist, set value to unknown
+  }
+  
+  /**
+   * Accessor for email address.
+   * 
+   * @return the email address
+   */
+  public String getEmailAddress(){
+  	return this.emailAddress;
+  }
+  
+  /**
+   * Accessor for first name.
+   * 
+   * @return the users first name
+   */
+  public String getFirstName(){
+  	return this.firstName;
+  }
+  
+  /**
+   * Accessor for surname.
+   * 
+   * @return the users surname
+   */
+  public String getSurname(){
+  	return this.surname;
+  }
 }
