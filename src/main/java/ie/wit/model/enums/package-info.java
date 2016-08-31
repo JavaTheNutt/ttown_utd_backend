8beeda67de6a7d4ecd2001113ee12acabf144a97
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-08-31 10:51:11 +0100

    Created package-info.java for enums package

---
+++ src/main/java/ie/wit/model/enums/package-info.java
@@ -0,0 +1,6 @@
+/**
+* This package contains all of the Enumerations (enums) required by model elements. Currently only contains the Role enum.
+*
+* @author Joe Wemyss
+*/
+package ie.wit.model.enums;
