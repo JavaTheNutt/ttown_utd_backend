70dd13ba1484b26ccd09b44815f21a4e174bde5d
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-09-01 15:10:38 +0100

    Update DoctorEntity.java: added attributes

--- src/main/java/ie/wit/model/entity/DoctorEntity.java
+++ src/main/java/ie/wit/model/entity/DoctorEntity.java
@@ -5,7 +5,26 @@ package ie.wit.model.entity;
  *
  * @author Joe Wemyss
  */
+ @Entity
+ @Table(name = "doctor")
 public class DoctorEntity
 {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "surname")
+	private String surname;
+	
+	@Column(name = "street_address")
+	private String streetAddress;
+	
+	@Column(name = "town_address")
+	private String townAddress;
+	
+	@Column(name = "contact_number")
+	private String contactNumber;
 }
