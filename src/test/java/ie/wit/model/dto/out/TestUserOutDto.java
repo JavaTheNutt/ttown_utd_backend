100a498cc8c28e779703afda38f661364fb9a639
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-09-01 11:47:03 +0100

    Updated TestUserOutDto.java
    Improved code coverage

--- src/test/java/ie/wit/model/dto/out/TestUserOutDto.java
+++ src/test/java/ie/wit/model/dto/out/TestUserOutDto.java
@@ -25,15 +25,26 @@ public class TestUserOutDto
 	public void testCreate(){
 		UserEntity user = new UserEntity("joe.wemyss@gmail.com", "MeJoe", 1);
 		UserOutDto userOut  = new UserOutDto(user);
+		
 		UserEntity user2 = new UserEntity("me@me.com", "MeJoe", 2);
 		user2.setFirstName("Joe");
 		user2.setSurname("Wemyss");
 		UserOutDto userOut2 = new UserOutDto(user2);
+		
+		UserEntity user3 = new UserEntity("you@you.com", "MeJoe", 3);
+		UserOutDto userOut3 = new UserOutDto(user3);
+		
+		
 		assertEquals("User 1 email does not match", "joe.wemyss@gmail.com", userOut.getEmailAddress());
 		assertEquals("User 2 email does not match", "me@me.com", userOut2.getEmailAddress());
-		assertEquals("User 1 role does not match", Integer.valueOf(1), userOut.getRole());
+		
+		assertEquals("User 1 role does not match", "ADMIN", userOut.getRole());
+		assertEquals("User 2 role does not match", "WRITE", userOut2.getRole());
+		assertEquals("User 3 role does not match", "READ", user3Out.getRole());
+		
 		assertEquals("User 1 first name does not match", "Unknown", userOut.getFirstName());
 		assertEquals("User 2 first name does not match", "Joe", userOut2.getFirstName());
+		
 		assertEquals("User 1 surname does not match", "Unknown", userOut.getSurname());
 		assertEquals("User 2 surname does not match", "Wemyss", userOut2.getSurname());
 	}
