ec35c37e7115ef835f9924ac9dfd3477f838649e
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-09-01 11:52:04 +0100

    Update TestLoginDto: test creation

--- src/test/java/ie/wit/model/dto/in/TestLoginDto.java
+++ src/test/java/ie/wit/model/dto/in/TestLoginDto.java
@@ -1,10 +1,31 @@
 package ie.wit.model.dto.in;
+
+import ie.wit.model.entity.UserEntity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
 * This class tests the login data transfer object.
 *
 * @author Joe Wemyss
 */
 @RunWith(SpringRunner.class)
-public class TestLoginDto{
-
+@SpringBootTest
+public class TestLoginDto
+{
+	/**
+	 * This method will test the creation of a login data transfer object.
+	 */
+	@Test
+	public void testCreateLoginDto
+	{
+		LoginDto login = new LoginDto("joewemyss3@gmail.com", "IAmJoe");
+		assertEquals("Email does not match", "joewemyss3@gmail.com", login.getEmailAddress());
+		assertEquals("Password does not match", "IAmJoe", login.getPassword());
+	}
 }
