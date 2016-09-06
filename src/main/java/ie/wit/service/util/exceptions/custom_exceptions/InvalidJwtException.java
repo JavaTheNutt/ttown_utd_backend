de10af39484186f665cd9f613d71855f19182a01
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-09-06 12:47:55 +0100

    Update InvalidJwtException: built class

--- src/main/java/ie/wit/service/util/exceptions/custom_exceptions/InvalidJwtException.java
+++ src/main/java/ie/wit/service/util/exceptions/custom_exceptions/InvalidJwtException.java
@@ -1,6 +1,26 @@
 package ie.wit.service.util.exceptions.custom_exceptions;
 
+/**
+ * This is the exception that is thrown when a passed JWT is invalid
+ * 
+ * @author Joe Wemyss
+ */
 public class InvalidJwtException extends RuntimeException
 {
-
+	/**
+	 * {@inheritDoc}
+	 */
+	 @Override
+	public InvalidJwtException()
+	{
+		super("Invalid JWT! The JWT passed is not valid!");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	 @Override
+	public InvalidJwtException(String message){
+		super(message);
+	}
 }
