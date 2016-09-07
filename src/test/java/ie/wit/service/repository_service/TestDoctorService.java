1ded21ea6e33590568b77f31bcb74083bdf82740
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-09-07 13:38:53 +0100

    Update TestDoctorService: test update

--- src/test/java/ie/wit/service/repository_service/TestDoctorService.java
+++ src/test/java/ie/wit/service/repository_service/TestDoctorService.java
@@ -25,17 +25,21 @@ public class TestDoctorService
 		DoctorOutDto doctor = doctorService.getOneDoctor(1L);
 		assertTrue("First name does not match", doctor.getFirstName().isPresent());
 	}
+	
 	@Test
-	public void testInsert()
-	{
+	public void testUpdate(){
 		try{
 			DoctorInDto docIn = new DoctorInDto("James", "Drynan", "Low Street", null, "05123456");
 			DoctorOutDto docOut = doctorService.insertDoctor(docIn);
 			assertEquals("The first name does not match", "James", docOut.getFirstName());
+			DoctorUpdateDto docIn2 = new DoctorUpdateDto(docOut.getId(), "John", docOut.getSurname(), docOut.getStreetAddress(), docOut.getTownAddress(), docOut.getContactNumber());
+			DoctorOutDto docOut2 = doctorService.updateDoctor(docIn2);
+			assertEquals("The first name does not match", "John", docOut2.getFirstName());
+			assertEquals("The town address does not match", "Thomastown", docOut2.getTownAddress());
 		}catch(Exception e){
 			e.printStackTrace();
 		}finally{
 			doctorService.deleteDoctor(docOut.getId());
-		}
+		}	
 	}
 }
