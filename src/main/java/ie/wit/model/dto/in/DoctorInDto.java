adecabefbbf9bbbaf6786e2cc5dc35a3ebedfe83
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-09-07 10:02:17 +0100

    Update DoctorInDto.java: built skeleton

--- src/main/java/ie/wit/model/dto/in/DoctorInDto.java
+++ src/main/java/ie/wit/model/dto/in/DoctorInDto.java
@@ -1,5 +1,71 @@
 package ie.wit.model.dto.in;
 
-public class DoctorInDto{
+import org.hibernate.validator.constraints.NotEmpty;
+import ie.wit.model.entity.DoctorEntity;
 
+import javax.validation.constraints.NotNull;
+/**
+ * This class represents the inward data transfer object for the {@link DoctorEntity}
+ * 
+ * @author Joe Wemyss
+ */
+public class DoctorInDto
+{
+    //todo: extend this class for updating objects? sub class could also have primary key property?
+    /**
+     * The first name of the doctor
+     */
+    private String firstName;
+    
+    /**
+     * The surname of the doctor
+     */
+    private String surname;
+    
+    /**
+     * The street address of the doctor
+     */
+    private String streetAddress;
+    
+    /**
+     * The town address of the doctor
+     */
+    private String townAddress;
+    
+    /**
+     * The contact number of the doctor.
+     * Cannot be null or empty.
+     */
+    @NotNull
+    @NotEmpty
+    private String contactNumber;
+    
+    /**
+     * The default constructor
+     */
+    public DoctorInDto()
+    {
+        
+    } 
+    
+    /**
+     * Constructor
+     */
+    public DoctorInDto(String firstName, String surname, String streetAddress, String townAddress, String contactNumber)
+    {
+        this.firstName = firstName;
+        this.surname = surname;
+        this.streetAddress = streetAddress;
+        this.townAddress = townAddress == null ? townAddress : "Thomastown";
+        this.contactNumber = contactNumber;
+    }
+    
+    //todo: getters, setters and toString
+    /**
+     * Return the doctor in a form that JPA can persist to the database
+     */
+    public DoctorEntity getAsEntity()
+    {
+        return new DoctorEntity(firstName, surname, streetAddress, townAddress, contactNumber);
+    }
 }
