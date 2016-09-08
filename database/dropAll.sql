8450a82a2653718e10935c2d77fe964321fb13e5
Joe Wemyss <joewemyss3@gmail.com>
2016-09-08 08:29:03 +0100

    added drop tables script

---
+++ database/dropAll.sql
@@ -0,0 +1,9 @@
+DROP TABLE low_fee;
+DROP TABLE high_fee;
+DROP TABLE medical_note;
+DROP TABLE parent;
+DROP TABLE player;
+DROP TABLE family;
+DROP TABLE doctor;
+DROP TABLE user;
+DROP SCHEMA thomastown_utd_test;
