a15241f6368145c0e7d932d81158e4a8e59cd102
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-09-09 11:18:55 +0100

    Update FamilyInDto.java: built skeleton

--- src/main/java/ie/wit/model/dto/in/FamilyInDto.java
+++ src/main/java/ie/wit/model/dto/in/FamilyInDto.java
@@ -1,5 +1,58 @@
 package ie.wit.model.dto.in;
 
-public class FamilyDtoIn{
-
+public class FamilyInDto{
+	@NotNull
+	@NotEmpty
+	private String familyName;
+	private String streetAddress;
+	private String townAddress;
+	private Float paidSoFar;
+	private Boolean willingToVolunteer;
+	private Long doctor;
+	
+	//todo constructor, getters and setters
+	public FamilyInDto(){}
+	
+	public String getFamilyName(){
+		return familyName;
+	}
+	public void setFamilyName(String familyName){
+		this.familyName = familyName;
+	}
+	public String getStreetAddress(){
+		return streetAddress;
+	}
+	public void setStreetAddress(String streetAddress){
+		this.streetAddress = streetAddress;
+	}
+	public String getTownAddress(){
+		return townAddress;
+	}
+	public void setTownAddress(String townAddress){
+		this.townAddress = townAddress;
+	}
+	public Float getPaidSoFar(){
+		return paidSoFar >= 0.0 ? paidSoFar : 0.0;
+	}
+	public void makePayment(Float amount){
+		paidSoFar += amount;
+	}
+	public void setPaidSoFar(Float paidSoFar){
+		this.paidSoFar = paidSoFar;
+	}
+	public Boolean getWillingToVolunteer(){
+		return willingToVolunteer != null ? willingToVolunteer : false;
+	}
+	public void setWillingToVolunteer(Boolean willingToVolunteer){
+		this.willingToVolunteer = willingToVolunteer;
+	}
+	public Long getDoctor(){
+		return doctor;
+	}
+	public void setDoctor(Long doctor){
+		this.doctor = doctor;
+	}
+	public FamilyEntity getAsEntity(){
+		return new FamilyEntity(this.familyName, this.streetAddress, this.townAddress, this.paidSoFar, this.willingToVolunteer, this.doctor);
+	}
 }
