5c5fe8bea2bece4a54f325f48e133f8139b85ac6
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-09-09 10:15:59 +0100

    Update FamilyEntity.java: added attrributes

--- src/main/java/ie/wit/model/entity/FamilyEntity.java
+++ src/main/java/ie/wit/model/entity/FamilyEntity.java
@@ -1,5 +1,56 @@
 package ie.wit.model.entity;
-
+/**
+ * This class represents a family that JPA will persist to the database.
+ * 
+ * @author Joe Wemyss
+ */
+@Entity
+@Table(name = "family")
 public class FamilyEntity{
-
+	/**
+	 * Entity primary key
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	/**
+	 * The name of the family
+	 */
+	@Column(name = "family_name")
+	@Basic(optional = false)
+	private String familyName;
+	
+	/**
+	 * The street address of the family
+	 */
+	@Column(name = "street_address")
+	private String streetAddress;
+	
+	/**
+	 * The town address of the family
+	 */
+	@Column(name = "town_address")
+	private String townAddress;
+	
+	/**
+	 * How much of the fee has been paid so far
+	 */
+	@Column(name = "paid_so_far")
+	private Float paidSoFar;
+	
+	/**
+	 * If the family is willing to volunteer
+	 */
+	@Column(name = "willing_to_volunteer")
+	private Boolean willingToVolunteer;
+	
+	/**
+	 * The foreign key for the one to many relationship with the doctor
+	 */
+	@Column(name = "doctor")
+	private Long doctor;
+	
+	//todo constructor, getters and setters 
+	
 }
